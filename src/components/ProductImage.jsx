ab130c8ab165fd80da9ab80bbf4437da765b83a3
Carolina <carolinazapatavidal@gmail.com>
2022-08-23 23:04:18 -0400

    productPrice update

--- src/components/ProductImage.jsx
+++ src/components/ProductImage.jsx
@@ -1,16 +1,9 @@
-import React from 'react';
+import React from "react";
 
-export const ProductImage = ({product}) => {
-    
-    return (
+export const ProductImage = ({ product }) => {
+  return (
     <div>
-        
-        
-         {Array.isArray(product?.images) ? console.log('holis', product.images) : ''}
-        <img src={product.images[0]}  />
+      <img src={product.images[0]} />
     </div>
-     
-    
-)
-  
-}
+  );
+};
