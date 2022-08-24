21aabe605f34e6a478e2e380dd002948f6b58c72
Noemí Flores <noemifloryaga@gmail.com>
2022-08-24 15:56:25 -0400

    ProductPrice-check

--- src/components/ProductData.jsx
+++ src/components/ProductData.jsx
@@ -4,8 +4,8 @@ import { AiOutlineShareAlt } from "react-icons/ai";
 export const ProductData = ({product}) => {
   return (
     <div className='w-[450px]'>
-        <p className='text-xs ml-[60%] text-gray-500'>Código del producto: {product.productId}</p>
-        <p className='text-sm font-bold'>{product.brand}</p>
+        <p className='text-sm font-bold inline'>{product.brand}</p>
+        <p className='text-xs ml-[40%] text-gray-500 inline'>Código del producto: {product.productId}</p>
         <p className='text-xl inline-block mr-48 font-light leading-8 tracking-tighter' >{product.name}</p> <AiOutlineShareAlt style={{color:'gray', display:'inline'}} />
 
     </div>
