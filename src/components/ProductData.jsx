1444571237351fc439dbe7f35175d148a7b9f024
Javiera <jbkc_10@hotmail.com>
2022-08-24 12:24:29 -0400

    product data margins

--- src/components/ProductData.jsx
+++ src/components/ProductData.jsx
@@ -3,10 +3,10 @@ import { AiOutlineShareAlt } from "react-icons/ai";
 
 export const ProductData = ({product}) => {
   return (
-    <div className='w-[450px]'>
+    <div className='w-[450px] ml-[5%]'>
         <p className='text-xs ml-[60%]'>Código del producto: {product.productId}</p>
         <p className='text-sm'>{product.brand}</p>
-        <p className='text-xl inline-block mr-48' >{product.name}</p> <AiOutlineShareAlt style={{color:'gray', display:'inline'}} />
+        <p className='text-xl inline-block mr-[20%]' >{product.name}</p> <AiOutlineShareAlt style={{color:'gray', display:'inline'}} />
 
     </div>
   )
