0483471892749da4fc4c9a51dbe9a8ceff60ba3b
Javiera <jbkc_10@hotmail.com>
2022-08-24 22:20:55 -0400

    div missing

--- src/components/ProductData.jsx
+++ src/components/ProductData.jsx
@@ -1,18 +1,17 @@
 import React from 'react'
 import { AiOutlineShareAlt } from "react-icons/ai";
 
-export const ProductData = ({product}) => {
+export const ProductData = ({ product }) => {
   return (
-
-    <div className='font-Lato w-[650px]'>
+    <div>
+      <div className='font-Lato w-[650px]'>
         <p className='text-sm font-bold inline'>{product.brand}</p>
         <p className='text-xs ml-[40%] text-gray-500 inline'>Código del producto: {product.productId}</p>
       </div>
       <div>
-        <p className='text-xl inline-block mr-48 font-light leading-8 tracking-tighter' >{product.name}</p> 
-        <AiOutlineShareAlt style={{color:'gray', display:'inline'}} />
+        <p className='text-xl inline-block mr-48 font-light leading-8 tracking-tighter' >{product.name}</p>
+        <AiOutlineShareAlt style={{ color: 'gray', display: 'inline' }} />
       </div>
-        
     </div>
   )
 }
