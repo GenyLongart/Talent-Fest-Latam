cdd90fba64df842f23452189b14d0b4a1749b9ba
Constanza <c.leytonleiva@gmail.com>
2022-08-25 12:33:52 -0400

    header

--- src/components/ProductData.jsx
+++ src/components/ProductData.jsx
@@ -4,12 +4,12 @@ import { AiOutlineShareAlt } from "react-icons/ai";
 export const ProductData = ({ product }) => {
   return (
     <div>
-      <div className='font-Lato w-[550px]'>
+      <div className='flex justify-between font-Lato w-[550px]'>
         <p className='text-sm font-bold inline'>{product.brand}</p>
         <p className='text-xs ml-[30%] text-gray-500 inline'>Código del producto: {product.productId}</p>
       </div>
-      <div>
-        <p className='text-xl inline-block mr-48 font-light leading-8 tracking-tighter' >{product.name}</p>
+      <div className='flex justify-between'>
+        <p className='text-[28px]  inline-block mr-48 font-light leading-8 tracking-tighter' >{product.name}</p>
         <AiOutlineShareAlt style={{ color: 'gray', display: 'inline' }} />
       </div>
     </div>
