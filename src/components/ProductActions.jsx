47805727832439a2e702dc16d22268aeb9240989
Javiera <jbkc_10@hotmail.com>
2022-08-25 16:28:34 -0400

    product actions button div

--- src/components/ProductActions.jsx
+++ src/components/ProductActions.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 export const ProductActions = () => {
   return (
-    <div className="flex">
-    <button className="h-[56px] w-[300px] rounded-full pr-10 pl-10 pt-4 pb-4 bg-[#FF6200] text-white grid gap-2.5 mt-20 font-bold text-lg text-center"> Agregar al Carro</button>
+    <div className="flex w-[300px] h-[150px] mt-[15px]">
+    <button className="h-[56px] w-[300px] rounded-full pr-10 pl-10 pt-4 pb-4 bg-[#FF6200] text-white grid gap-2.5 font-bold text-lg text-center"> Agregar al Carro</button>
     </div>
   );
 };
