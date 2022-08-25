315e8975ce07ab2aeeb74a5ce2ba977efdb72e50
Constanza <c.leytonleiva@gmail.com>
2022-08-24 22:14:52 -0400

    header icons

--- src/components/ProductData.jsx
+++ src/components/ProductData.jsx
@@ -5,9 +5,15 @@ export const ProductData = ({product}) => {
   return (
 
     <div className='font-Lato w-[450px]'>
+      <div>
         <p className='text-sm font-bold inline'>{product.brand}</p>
         <p className='text-xs ml-[40%] text-gray-500 inline'>Código del producto: {product.productId}</p>
-        <p className='text-xl inline-block mr-48 font-light leading-8 tracking-tighter' >{product.name}</p> <AiOutlineShareAlt style={{color:'gray', display:'inline'}} />
+      </div>
+      <div>
+        <p className='text-xl inline-block mr-48 font-light leading-8 tracking-tighter' >{product.name}</p> 
+        <AiOutlineShareAlt style={{color:'gray', display:'inline'}} />
+      </div>
+        
     </div>
   )
 }
